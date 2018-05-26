0af93f28a2df59c8580f9f5aec580093161230f9
vickris <christophervundi@gmail.com>
2018-05-26 15:43:26 +0300

    Create skills template

---
+++ src/components/Skills.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <div :class="alertObject">
+      <h1>{{ message }}</h1>
+      <button @click.prevent="changeName" :disabled="btnState">Some text</button>
+      <ul>
+        <li v-for="(skill, index) in skills" :key="index">{{ skill.name }} is at index {{ index }}</li>
+      </ul>
+      <p v-if="skills.length >= 1">You have more than one skill</p>
+      <p v-else>You need to amass more skills</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data () {
+    return {
+      skills: [
+        { name: "Javeling" },
+        { name: "Plumbing" },
+      ],
+      message: "Some text goes in here",
+      alertObject: {
+        alert: true
+      }
+
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="./styles/skills.css" scoped>
+
+</style>
